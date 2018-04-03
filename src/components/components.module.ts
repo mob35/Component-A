b133f2b074cb7d434f105e363023f4a9c2f1b224
mob35 <toonelicious@gmail.com>
2018-04-03 16:30:26 +0700

    update

---
+++ src/components/components.module.ts
@@ -0,0 +1,8 @@
+import { NgModule } from '@angular/core';
+import { ComponentAComponent } from './component-a/component-a';
+@NgModule({
+	declarations: [ComponentAComponent],
+	imports: [],
+	exports: [ComponentAComponent]
+})
+export class ComponentsModule {}
