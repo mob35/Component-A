53311b84870eae788606957644c57a9104e15b05
mob35 <toonelicious@gmail.com>
2018-04-03 16:34:09 +0700

    update

---
+++ src/components/component-a/component-a.ts
@@ -0,0 +1,22 @@
+import { Component } from '@angular/core';
+
+/**
+ * Generated class for the ComponentAComponent component.
+ *
+ * See https://angular.io/api/core/Component for more info on Angular
+ * Components.
+ */
+@Component({
+  selector: 'component-a',
+  templateUrl: 'component-a.html'
+})
+export class ComponentAComponent {
+
+  text: string;
+
+  constructor() {
+    console.log('Hello ComponentAComponent Component');
+    this.text = 'Hello World';
+  }
+
+}
